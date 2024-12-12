47d9491b5d83ff12442fb4b47555ba570e73f2ed
Danil Novak <novak.da@yandex.ru>
2024-12-12 21:38:16 +0300

    add css&adaptive for footer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-console.log(2)
+/* footer */
+document.addEventListener("DOMContentLoaded", (event) => {
+  const recaptcha = document.querySelector(".g-recaptcha");
+  recaptcha.setAttribute("data-theme", "dark");
+});
